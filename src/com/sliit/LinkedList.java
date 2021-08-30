e2c7aa2423bdb079eed9b9d233e4161f708b7a49
Flashminat0 <wyasaslowe@gmail.com>
2021-08-30 07:26:48 +0530

    Done all XD

--- src/com/sliit/LinkedList.java
+++ src/com/sliit/LinkedList.java
@@ -61,5 +61,24 @@ public class LinkedList {
         }
     }
 
+    public static void main(String[] args) {
+
+        //Linked Lists
+        /* Start with the empty list. */
+        LinkedList list = new LinkedList();
+        // INSERTION
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
 
--- src/com/sliit/Stack.java
+++ src/com/sliit/Stack.java
@@ -56,4 +56,18 @@ class Stack {
             System.out.print(" "+ a[i]);
         }
     }
+
+    public static void main(String[] args) {
+        //Stack
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println("Top element is :" + s.peek());
+        System.out.print("Elements present in stack :");
+        s.print();
+
+
+    }
 }
